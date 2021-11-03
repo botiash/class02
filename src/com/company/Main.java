824012fcb52187b3eef89e1d15a244b4dddcdcab
botiash <92024399+botiash@users.noreply.github.com>
2021-11-03 16:06:07 +0600

    Initial commit f

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        bank b[] = new bank[4];
+    b[0] = new bank(1,2,3,"g","h");
+    b[1]=new bank (1,4,5,"hgf","sdfgh");
+        b[2]=new bank (2,7,6,"hdfghj","dbftjuyk");
+        b[3]=new bank (12,456,545,"hdfgh","sdryfgh");
+int i,j; // intervalga kazhet ainimalilar
+        System.out.println("interval engiz");
+i=sc.nextInt();
+        j=sc.nextInt();
+        for (int k=0; k<4;k++) {
+            if (b[i].getCredit()>i && b[i].getCredit()>j) System.out.println(b[k].toString());
+        }
+    }
+}
---
+++ src/com/company/bank.java
@@ -0,0 +1,59 @@
+package com.company;
+
+public class bank {
+    public int id, schet, credit;
+    public String FIO, adress;
+
+    public bank(int id, int schet, int credit, String FIO, String adress) {
+        this.id = id;
+        this.schet = schet;
+        this.credit = credit;
+        this.FIO = FIO;
+        this.adress = adress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSchet() {
+        return schet;
+    }
+
+    public void setSchet(int schet) {
+        this.schet = schet;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public String getFIO() {
+        return FIO;
+    }
+
+    public void setFIO(String FIO) {
+        this.FIO = FIO;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+}
+
+
+
+
+
